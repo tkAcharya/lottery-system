20b95d26056e3a93e29faab00f3dfee72173c53b
tkacharya <>
2020-12-07 00:25:50 +0530

    Added compile.js file

--- lottery/compile.js
+++ lottery/compile.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const solc = require('solc');
 
 
-const inboxPath = path.resolve(__dirname,'contracts','Lottery.sol');
-const source = fs.readFileSync(inboxPath,'utf8');
+const lotteryPath = path.resolve(__dirname,'contracts','Lottery.sol');
+const source = fs.readFileSync(lotteryPath,'utf8');
 
 //Compilation of the code
 //console.log(solc.compile(source,1)); //1 here defines how many different number of contracts to compile
