e3142b48bf758608c54368a177370e18cbcecfe3
tkacharya <>
2020-12-10 23:26:33 +0530

    Validated the deployment layer

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -6,11 +6,33 @@ const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 const {interface, bytecode} = require('../compile');
 
+let accounts;
+let contractObject;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  contractObject = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000'})
+});
+
+
+describe('Lottery Tests', () => {
+  it('Verifying the Deployment',() => {
+    console.log(accounts);
+    assert.ok(contractObject.options.address);
+  });
+
+  // it('Registering a player',async () => {
+  //   await contractObject.methods.entry().send({ from: accounts[0]});
+  //   const message = await inbox.methods.message().call();
+  //   assert.equal(message , localMessage);
+  //
+  // });
+
+
+
+
+
 });
